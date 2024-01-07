4b9e8b0cad70525d48310a7692774731558e8dd5
Smilla <smillapostb@gmail.com>
2024-01-08 00:00:27 +0100

    rettelser

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const active = document.getElementById("active-menu");
+
+  menuIcon.addEventListener("click", function () {
+    active.classList.toggle("menu-opened");
+  });
+});
